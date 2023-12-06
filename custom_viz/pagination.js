01d92b6d620dd88713a2a0343ccecc97ee03e8ee
Monil Shah <monil.shah@crestdatasys.com>
2023-12-06 06:18:44 +0000

    Modified the pagination code.

--- custom_viz/pagination.js
+++ custom_viz/pagination.js
@@ -19,41 +19,30 @@ looker.plugins.visualizations.add({
       this.table.style.marginTop = '20px';
 
       var headerRow = this.table.insertRow(0);
-      headerRow.insertCell(0).textContent = 'Category Name';
-      headerRow.insertCell(1).textContent = 'Category ID';
+      headerRow.insertCell(0).textContent = 'Order Number';
+      headerRow.insertCell(1).textContent = 'Order Status';
 
       // Initialize page number
       this.currentPage = 1;
 
-      // Fetch data and update the table
-      this.updateAsync();
+      this.rowsPerPage = 10;
     },
 
-    updateAsync: function(data, element, config, queryResponse, details, done) {
-      // ... (existing code)
-      console.log('Function Called..')
-
+    paginate : function(){
       // Extract data from Looker's query response
       var col1 = queryResponse.fields.dimension_like[0].name;
       var col2 = queryResponse.fields.dimension_like[1].name;
 
-      // Define the number of rows per page
-      var rowsPerPage = 4;
-      console.log(this.currentPage);
       // Calculate the start and end indices based on the current page
-      var startIndex = (this.currentPage - 1) * rowsPerPage;
-      var endIndex = startIndex + rowsPerPage;
+      var startIndex = (this.currentPage - 1) * this.rowsPerPage;
+      var endIndex = startIndex + this.rowsPerPage;
 
       // Clear existing rows and page numbers
       while (this.table.rows.length > 1) {
         this.table.deleteRow(1);
       }
-      this.pageNumbersContainer.innerHTML = '';
-      // console.log("Data Length: ", data.length);
-      console.log(startIndex,endIndex);
       // Populate the table with data for the current page
       for (var i = startIndex; i < Math.min(endIndex, data.length); ++i) {
-        // ... (existing code)
           // var row = this.table.insertRow(i+1);
           var row = this.table.insertRow(i-startIndex+1);
           var cell1 = row.insertCell(0);
@@ -62,32 +51,28 @@ looker.plugins.visualizations.add({
           cell1.textContent = data[i][col1].value;
           cell2.textContent = data[i][col2].value;
       }
+    },
+
+    updateAsync: function(data, element, config, queryResponse, details, done) {
+
+      this.paginate()
 
       // Add page numbers
-      var totalPages = Math.ceil(data.length / rowsPerPage);
-      // console.log("Total Pages", totalPages);
-      for (var page = 1; page <= totalPages; page++) {
-        var pageNumberElement = document.createElement('span');
+      var totalPages = Math.ceil(data.length / this.rowsPerPage);
+      for (let page = 1; page <= totalPages; ++page) {
+      var pageNumberElement = document.createElement('span');
+
         pageNumberElement.textContent = page;
         pageNumberElement.style.marginRight = '5px';
         pageNumberElement.style.cursor = 'pointer';
-        pageNumberElement.onclick = (function (pageNum) {
-          return function () {
-            console.log('Clicked on page number:', pageNum);
-            this.currentPage = pageNum;
-            console.log('Updated currentPage:', this.currentPage);
-            this.updateAsync();
-          }.bind(this);
-        })(page);
-        // pageNumberElement.onclick = (pageNum) => {
-        //   console.log('Clicked on page number:', pageNum);
-        //   this.currentPage = pageNum;
-        //   console.log('Updated currentPage:', this.currentPage);
-        //   this.updateAsync();
-        // };
+        pageNumberElement.id = page;
+        pageNumberElement.onclick =  (evt) => {
+          this.currentPage = evt.target.id;
+          console.log('Clicked on page number: ', this.currentPage);
+          this.paginate();
+        };
         this.pageNumbersContainer.appendChild(pageNumberElement);
       }
-
       // Signal the completion of rendering
       done();
     }
